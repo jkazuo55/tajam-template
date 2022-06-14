8da3346f66bad38097adf78cbd4af0de8e17dabe
jkazuo55 <jkazuo555@gmail.com>
2022-06-14 16:37:25 -0400

    jquery added

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+// // $(".slider").slick({
+
+
+// // });
+
+
+// // 👇️ using noConflict mode
+// const $jq = jQuery.noConflict();
+
+// $jq(document).ready(function() {
+//     $jq('.slider').slick({
+//         dots: true,
+//         centerMode: true,
+
+//         // normal options...
+//         infinite: false,
+
+//         // the magic
+//         responsive: [{
+
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 infinite: true
+//             }
+
+//         }, {
+
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 2,
+//                 dots: true
+//             }
+
+//         }, {
+
+//             breakpoint: 300,
+//             settings: "unslick" // destroys slick
+
+//         }]
+//     });
+// });
+
+
+
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true,
+        centerMode: true,
+    });
+});
